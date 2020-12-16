f70946998ac6af042fca5532476933cc6c75c79d
Matt Janda <matt.janda.kingston@gmail.com>
2020-12-16 10:04:12 +0000

    chore: Update dictionary demo

--- src/Janda.CTF.Dictionary.Demo/DictionaryDemo.cs
+++ src/Janda.CTF.Dictionary.Demo/DictionaryDemo.cs
@@ -1,22 +1,36 @@
-﻿namespace Janda.CTF.Dictionary.Demo
+﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Janda.CTF.Dictionary.Demo
 {
     internal class DictionaryDemo : IDictionaryDemo
     {
         readonly IDictionaryService _dictionary;
+        readonly ILogger<DictionaryDemo> _logger;
 
-        public DictionaryDemo(IDictionaryService dictionary)
+        public DictionaryDemo(IDictionaryService dictionary, ILogger<DictionaryDemo> logger)
         {
             _dictionary = dictionary;
+            _logger = logger;
         }
 
         public void Run()
         {
-            var words = _dictionary.GetWords();    
+            var words = _dictionary.GetWords();
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
 
             foreach (var word in words)
-            {
+            {            
+                if (!word.StartsWith('#'))
+                {
 
-            }
+                }
+            }            
+
+
+            _logger.LogTrace("Word enumeration completed in {stopwatch}", stopwatch.Elapsed);
         }
     }
 }
--- src/Janda.CTF.Dictionary.Demo/Program.cs
+++ src/Janda.CTF.Dictionary.Demo/Program.cs
@@ -6,22 +6,10 @@ namespace Janda.CTF.Dictionary.Demo
     {
         static void Main(string[] args)
         {
+            WorkbenchConsole.Run<IDictionaryDemo, DictionaryDemo>(args, (service) => service.Run(), 
+               (services) => services.AddDictionary());
 
-            // No error is reported !!!
-            // WorkbenchConsole.Run<IDictionaryDemo, DictionaryDemo>((service) => service.Run());
-
-
-            // Exception thrown: 'System.InvalidOperationException' in Microsoft.Extensions.DependencyInjection.dll
-            WorkbenchConsole.Run<IDictionaryDemo, DictionaryDemo>((service) => service.Run(), 
-                (services) => services.AddDictionaryServices());
-
-
-            //new WorkbenchConsole()
-            //    .WithServices((services) => services
-            //        .AddTransient<IDictionaryDemo, DictionaryDemo>()
-            //        .AddDictionary())
-
-            //    .Run((provider) => provider.GetRequiredService<IDictionaryDemo>().Run());
         }
     }
 }
+
