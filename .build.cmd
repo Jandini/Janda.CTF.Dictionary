b9f75fa1b9195e7587ad4f1fda4b375bea4efcb5
Matt Janda <matt.janda.kingston@gmail.com>
2021-08-02 12:22:42 +0100

    chore: Update dependencies

--- .build.cmd
+++ .build.cmd
@@ -2,7 +2,7 @@
 :: This build overrides dots build
 dotnet build -c Release src\Janda.CTF.Dictionary.Builder
 cd bin\Release\netcoreapp3.1
-Janda.CTF.Dictionary.Builder.exe --name B001
+Janda.CTF.Dictionary.Builder.exe --class B001
 cd ..\..\..
 dotnet build src\Janda.CTF.Dictionary.sln
 
